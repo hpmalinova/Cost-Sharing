23d58dcc3e338400e4a4473afd31c71979c60ca5
hpmalinova <hrisi.pm@abv.bg>
2021-02-16 20:32:23 +0200

    Move

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"Cost-Sharing/server"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,36 +8,33 @@ import (
 
 func main() {
 	fmt.Println("Launch server..")
-	app := server.InitApp()
+	app := InitApp()
 
 	// # Create or Login
-	app.Server.Router.HandleFunc(server.PathToCreateAccount, app.CreateAccount).Methods("POST")
-	app.Server.Router.HandleFunc(server.PathToLogin, app.Login).Methods("POST")
+	app.Server.Router.HandleFunc(PathToCreateAccount, app.CreateAccount).Methods("POST")
+	app.Server.Router.HandleFunc(PathToLogin, app.Login).Methods("POST")
 
 	// # Users
-	app.Server.Router.HandleFunc(server.PathToShowUsers, server.Notify(&app, app.ShowUsers)).Methods("GET")
+	app.Server.Router.HandleFunc(PathToShowUsers, Notify(&app, app.ShowUsers)).Methods("GET")
 
 	// # Friends
-	app.Server.Router.HandleFunc(server.PathToAddFriend, server.Notify(&app, app.AddFriend)).Methods("POST")
-	app.Server.Router.HandleFunc(server.PathToShowFriends, server.Notify(&app, app.ShowFriends)).Methods("GET")
+	app.Server.Router.HandleFunc(PathToAddFriend, Notify(&app, app.AddFriend)).Methods("POST")
+	app.Server.Router.HandleFunc(PathToShowFriends, Notify(&app, app.ShowFriends)).Methods("GET")
+
+	app.Server.Router.HandleFunc(PathToAddDebt, Notify(&app, app.AddDebtToFriend)).Methods("POST")
+
+	app.Server.Router.HandleFunc(PathToShowDebts, Notify(&app, app.ShowDebts)).Methods("GET")
+	app.Server.Router.HandleFunc(PathToShowLoans, Notify(&app, app.ShowLoans)).Methods("GET")
 
 	// # Groups
-	app.Server.Router.HandleFunc(server.PathToCreateGroup, server.Notify(&app, app.CreateGroup)).Methods("POST")
-	app.Server.Router.HandleFunc(server.PathToShowGroups, server.Notify(&app, app.ShowGroups)).Methods("GET")
+	app.Server.Router.HandleFunc(PathToCreateGroup, Notify(&app, app.CreateGroup)).Methods("POST")
+	app.Server.Router.HandleFunc(PathToShowGroups, Notify(&app, app.ShowGroups)).Methods("GET")
 
-	// # Debt
-	// ## To friends
-	app.Server.Router.HandleFunc(server.PathToAddDebt, server.Notify(&app, app.AddDebtToFriend)).Methods("POST")
+	app.Server.Router.HandleFunc(PathToAddDebtToGroup, Notify(&app, app.AddDebtToGroup)).Methods("POST")
+	app.Server.Router.HandleFunc(PathToReturnDebt, Notify(&app, app.ReturnDebt)).Methods("POST")
 
-	app.Server.Router.HandleFunc(server.PathToShowDebts, server.Notify(&app, app.ShowDebts)).Methods("GET")
-	app.Server.Router.HandleFunc(server.PathToShowLoans, server.Notify(&app, app.ShowLoans)).Methods("GET")
+	app.Server.Router.HandleFunc(PathToShowDebtsToGroups, Notify(&app, app.ShowDebtsToGroups)).Methods("GET")
+	app.Server.Router.HandleFunc(PathToShowLoansToGroups, Notify(&app, app.ShowLoansToGroups)).Methods("GET")
 
-	// ## To group
-	app.Server.Router.HandleFunc(server.PathToAddDebtToGroup, server.Notify(&app, app.AddDebtToGroup)).Methods("POST")
-	app.Server.Router.HandleFunc(server.PathToReturnDebt, server.Notify(&app, app.ReturnDebt)).Methods("POST")
-
-	app.Server.Router.HandleFunc(server.PathToShowDebtsToGroups, server.Notify(&app, app.ShowDebtsToGroups)).Methods("GET")
-	app.Server.Router.HandleFunc(server.PathToShowLoansToGroups, server.Notify(&app, app.ShowLoansToGroups)).Methods("GET")
-
-	log.Fatal(http.ListenAndServe(server.Port, app.Server.Router))
+	log.Fatal(http.ListenAndServe(Port, app.Server.Router))
 }
